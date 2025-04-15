e529509da78ff4e53b31f02cfa99f45b8bdece0c
pleszkowska <p.leszkowska.998@studms.ug.edu.pl>
2025-04-15 16:01:39 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello user!");
+        System.out.printf("Hello user!\n");
 
-        for (int i = 1; i <= 16; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+       String[] names = {"Piotr", "Anna", "Jan", "Leszek"};
+
+       for (int i = 1; i<=4; i++) {
+           System.out.println(i + ". "+ names[i - 1]);
+
+       }
     }
 }
